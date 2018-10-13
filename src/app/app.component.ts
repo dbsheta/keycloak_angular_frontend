f4cddf296ee4eccc11c151f3e4095f8bf1ea9fe5
Dhoomil Sheta(dhoomil.sheta) <dhoomil.sheta@infosys.com>
2018-10-13 15:55:35 +0530

    token view for admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DemoService } from './demo.service';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,19 @@ import { DemoService } from './demo.service';
 export class AppComponent {
   title = 'app';
   msg = 'Waiting for server...';
+  token = '';
 
-  constructor(private service: DemoService) {}
+  constructor(private service: DemoService, private auth: KeycloakService) {}
 
   // tslint:disable-next-line:use-life-cycle-interface
   ngOnInit() {
     console.log(this.msg);
     this.service.getUserMsg().subscribe(data => (this.msg = data['msg']));
+    this.getToken();
+    this.auth.isUserInRole('ROLE_ADMIN');
+  }
+
+  getToken() {
+    this.auth.getToken().then(data => (this.token = data));
   }
 }
